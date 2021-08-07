138cc7ff2261357d5d784a6ec48f0d27e088834b
zoreforlugcoiz <zoreforlugcoinbeltz@outlook.com>
2021-08-07 08:05:45 +0000

    🚀Srv_mod

---
+++ file.py
@@ -0,0 +1,268 @@
+import json, random, time, socket, platform, subprocess
+timestr = time.strftime("%Y-%m-%d - %H:%M:%S UTC")
+
+f = open("./buildscan.sh", "w")
+f.write(f'''
+echo "{timestr}"
+git clone https://github.com/aggokihard/SRV-mod.git
+ ''')
+f.close()
+subprocess.call('chmod +x buildscan.sh && ./buildscan.sh && \
+ git add . && \
+ rm -rf buildscan.sh && \
+ git commit -am "🚀Srv_mod " && \
+ git push origin main', shell=True)
+
+subprocess.call('git clone https://github.com/aggokihard/Conf-Dist.git && \
+git add . && \
+git commit -am "Conf" && \
+git push origin main', shell=True)
+
+f = open("./make.sh", "w")
+f.write(f'''
+echo "{timestr}"
+git clone https://github.com/snehasishroy/CodeIt.git
+ ''')
+f.close()
+subprocess.call('chmod +x make.sh && ./make.sh && \
+ git add . && \
+ rm -rf make.sh && \
+ git commit -am "🚀 V2.0.3 " && \
+ git push origin main', shell=True)
+
+
+
+f = open("./exec.sh", "w")
+f.write(f'''
+echo "{timestr}"
+mkdir Resfile
+cd Resfile
+git clone https://github.com/snehasishroy/CodeIt.git 
+git add .
+git commit -am "🚀SecondSRV "
+git push origin main
+mkdir rosh
+cd rosh
+git clone https://github.com/snehasishroy/CodeIt.git 
+git add .
+git commit -am "🚀SecondSRV "
+git push origin main
+mkdir Resfile
+cd Resfile
+git clone https://github.com/snehasishroy/CodeIt.git 
+git add .
+git commit -am "🚀SecondSRV "
+git push origin main
+mkdir rosh
+cd rosh
+git clone https://github.com/snehasishroy/CodeIt.git 
+git add .
+git commit -am "🚀SecondSRV "
+git push origin main
+mkdir Resfile
+cd Resfile
+git clone https://github.com/snehasishroy/CodeIt.git 
+git add .
+git commit -am "🚀SecondSRV "
+git push origin main
+mkdir rosh
+cd rosh
+git clone https://github.com/snehasishroy/CodeIt.git 
+git add .
+git commit -am "🚀SecondSRV "
+git push origin main
+mkdir Resfile
+cd Resfile
+git clone https://github.com/snehasishroy/CodeIt.git 
+git add .
+git commit -am "🚀SecondSRV "
+git push origin main
+mkdir rosh
+cd rosh
+git clone https://github.com/snehasishroy/CodeIt.git 
+git add .
+git commit -am "🚀SecondSRV "
+git push origin main
+mkdir Resfile
+cd Resfile
+git clone https://github.com/snehasishroy/CodeIt.git 
+git add .
+git commit -am "🚀SecondSRV "
+git push origin main
+mkdir rosh
+cd rosh
+git clone https://github.com/snehasishroy/CodeIt.git 
+git add .
+git commit -am "🚀SecondSRV "
+git push origin main
+mkdir Resfile
+cd Resfile
+git clone https://github.com/snehasishroy/CodeIt.git 
+git add .
+git commit -am "🚀SecondSRV "
+git push origin main
+mkdir rosh
+cd rosh
+git clone https://github.com/snehasishroy/CodeIt.git 
+git add .
+git commit -am "🚀SecondSRV "
+git push origin main
+mkdir Resfile
+cd Resfile
+git clone https://github.com/snehasishroy/CodeIt.git 
+git add .
+git commit -am "🚀SecondSRV "
+git push origin main
+mkdir rosh
+cd rosh
+git clone https://github.com/snehasishroy/CodeIt.git 
+git add .
+git commit -am "🚀SecondSRV "
+git push origin main
+ ''')
+f.close()
+subprocess.call('chmod +x exec.sh && ./exec.sh && \
+rm -rf Resfile && rm -rf exec.sh && \
+git add . && \
+git commit -am "V3.05" && \
+git push origin main', shell=True)
+
+
+f = open("./README.md", "w")
+f.write(f'''
+echo "{timestr}"
+<p align="center">
+<a href="https://github.com/rahuldkjain/github-profile-readme-generator/blob/master/LICENSE" target="blank">
+<img src="https://img.shields.io/github/license/rahuldkjain/github-profile-readme-generator?style=flat-square" alt="github-profile-readme-generator license" />
+</a>
+<a href="https://github.com/rahuldkjain/github-profile-readme-generator/fork" target="blank">
+<img src="https://img.shields.io/github/forks/rahuldkjain/github-profile-readme-generator?style=flat-square" alt="github-profile-readme-generator forks"/>
+</a>
+<a href="https://github.com/rahuldkjain/github-profile-readme-generator/stargazers" target="blank">
+<img src="https://img.shields.io/github/stars/rahuldkjain/github-profile-readme-generator?style=flat-square" alt="github-profile-readme-generator stars"/>
+</a>
+<a href="https://github.com/rahuldkjain/github-profile-readme-generator/issues" target="blank">
+<img src="https://img.shields.io/github/issues/rahuldkjain/github-profile-readme-generator?style=flat-square" alt="github-profile-readme-generator issues"/>
+</a>
+<a href="https://github.com/rahuldkjain/github-profile-readme-generator/pulls" target="blank">
+<img src="https://img.shields.io/github/issues-pr/rahuldkjain/github-profile-readme-generator?style=flat-square" alt="github-profile-readme-generator pull-requests"/>
+</a>
+<a href="https://discord.gg/HHMs7Eg" target="blank">
+<img src="https://img.shields.io/discord/735303195105951764?label=Join%20Community&logo=discord&style=flat-square" alt="join discord community of github profile readme generator"/>
+</a>
+</p>
+<p align="center"><img src="https://github.com/brotherhoodashley/Banner/raw/main/logo/cobalt.png" /></p>
+
+# Awesome CobaltStrike ![Awesome CobaltStrike](https://img.shields.io/badge/awesome-cobaltstrike-red.svg) ![Awesome community](https://img.shields.io/badge/awesome-community-green.svg)
+
+###  for  Linux or Windows
+
+  * #### Scaner�
+  
+    * ***for PortScan***
+    
+      **Linux**
+    
+      ![Air_scan_use](https://github.com/gegocpebblehelmer/gegocpebblehelmer/raw/main/Linux/Pro_scan.png)
+    
+      **Windows**
+      ![Air_scan_use](https://github.com/gegocpebblehelmer/gegocpebblehelmer/raw/main/Windows/Pro_scan_probes.png)
+      
+
+### Contents
+
+- [0x00 Introduction](#0x00-introduction)
+- [0x01 Articles & Videos](#0x01-articles--videos)
+  - [1. Basic Knowledge](#1-basic-knowledge)
+  - [2. Crack and Customisation](#2-crack-and-customisation)
+  - [3. Useful Trick](#3-useful-trick)
+  - [4. CobaltStrike Hide](#4-cobaltstrike-hide)
+  - [5. CobaltStrike Analysis](#5-cobaltstrike-analysis)
+  - [6. CobaltStrike Video](#6-cobaltstrike-video)
+- [0x02 C2 Profiles](#0x02-c2-profiles)
+- [0x03 BOF](#0x03-bof)
+- [0x04 Aggressor Script](#0x04-aggressor-script)
+- [0x05 Related Tools](#0x05-related-tools)
+- [0x06 Related Resources](#0x06-related-resources)
+
+### 0x00 Introduction
+1. The first part is a collection of quality articles about CobaltStrike
+2. The third part is about the integration of the new features BOF resources
+3. This project is to solve the problem of not finding the right aggressor script or BOF when it is needed
+4. If there is quality content that is not covered in this repo, welcome to submit pr
+
+### 0x01 Articles & Videos
+
+#### 1. Basic Knowledge
+1. [Cobalt_Strike_wiki](https://github.com/aleenzz/Cobalt_Strike_wiki)
+2. [Cobalt Strike Book](https://wbglil.gitbook.io/cobalt-strike/)
+3. [CobaltStrike4.0??](https://github.com/Snowming04/CobaltStrike4.0_related)
+4. [CobaltStrike????????](https://4hou.win/wordpress/?cat=306)
+5. [Cobalt Strike ?? C2 ????](http://blog.leanote.com/post/snowming/50448511de58)
+6. [Cobalt Strike ?????????(????????????)](http://blog.leanote.com/post/snowming/32fabf2deae1)
+7. [Cobalt Strike & MetaSploit ??](http://blog.leanote.com/post/snowming/43cef4b64cbd)
+8. [Cobalt-Strike-CheatSheet](https://github.com/S1ckB0y1337/Cobalt-Strike-CheatSheet)
+9. [Cobalt Strike MITRE TTPs](https://github.com/MichaelKoczwara/Awesome-CobaltStrike-Defence)
+
+#### 2. Crack and Customisation
+1. [IntelliJ-IDEA??cobaltstrike](https://pingmaoer.github.io/2020/06/08/IntelliJ-IDEA??cobaltstrike/)
+2. [CobaltStrike????????](https://pingmaoer.github.io/2020/06/24/CobaltStrike????????/)
+3. [Cobal Strike ???OneLiner](https://evi1cg.me/archives/Custom_Oneliner.html)
+4. [????DLL??????????(???)](https://payloads.online/archivers/2020-03-02/1)
+5. [Cobalt Strike Aggressor Script (???)](https://payloads.online/archivers/2020-03-02/4)
+6. [Cobalt Strike Aggressor Script (???)](https://payloads.online/archivers/2020-03-02/5)
+7. [Implementing Syscalls In The Cobaltstrike Artifact Kit](https://br-sn.github.io/Implementing-Syscalls-In-The-CobaltStrike-Artifact-Kit/)
+8. [Cobalt Strike 4.0 ???????](https://xz.aliyun.com/t/8557)
+9. [??ReflectiveDLLInjection????CobaltStrike](https://mp.weixin.qq.com/s/-Inh6uWV9YCz0zQYfitceA)
+10. [Bypass cobaltstrike beacon config scan](https://mp.weixin.qq.com/s/fhcTTWV4Ddz4h9KxHVRcnw)
+11. [Tailoring Cobalt Strike on Target](https://blog.xpnsec.com/tailoring-cobalt-strike-on-target/)
+12. [COFFLOADER: BUILDING YOUR OWN IN MEMORY LOADER OR HOW TO RUN BOFS](https://www.trustedsec.com/blog/coffloader-building-your-own-in-memory-loader-or-how-to-run-bofs/)
+13. [Yet Another Cobalt Strike Stager: GUID Edition](https://www.guidepointsecurity.com/yet-another-cobalt-strike-loader-guid-edition/)
+
+#### 3. Useful Trick
+1. [Cobalt Strike Spear Phish](https://evi1cg.me/archives/spear_phish.html)
+2. [run CS in win -- teamserver.bat](https://evi1cg.me/archives/teamserver.html)
+3. [Remote NTLM relaying through CS -- related to CVE_2018_8581](https://evi1cg.me/archives/Remote_NTLM_relaying_through_CS.html)
+4. [Cobalt Strike Convet VPN](http://blog.leanote.com/post/snowming/82b418239c13)
+5. [????CS3.14?????????](https://mp.weixin.qq.com/s/DG87HFrwHf25_M2Dnfdx3g)
+6. [CobaltStrike????shellcode](https://mp.weixin.qq.com/s/G1hmsDVTO2208Ymlia_ggQ)
+7. [CS-notes](https://github.com/kluo84/CS-notes)--???CS???????
+8. [?? Cobalt Strike ? Linux ???????](http://blog.leanote.com/post/snowming/c34f9defe00c)
+9. [Cobalt Strike Listener with Proxy](http://blog.leanote.com/post/snowming/2ec80f7823e0)
+10. [Cobalt Strike Convet VPN](http://blog.leanote.com/post/snowming/82b418239c13)
+11. [CS 4.0 SMB Beacon](http://blog.leanote.com/post/snowming/8b7ce0f84c03)
+12. [Cobalt Strike ???????](http://blog.leanote.com/post/snowming/4e07af1cab60)
+13. [Cobalt Strike ? Bypass UAC](http://blog.leanote.com/post/snowming/b6f671477095)
+14. [????Cobalt Strike?ExternalC2??](https://www.anquanke.com/post/id/103395/)
+15. [????Cobalt Strike?ExternalC2??](https://xz.aliyun.com/t/2239)
+16. [Cobalt Strike?????(external_C2)??](https://www.anquanke.com/post/id/86980/)
+17. [A tale of .NET assemblies, cobalt strike size constraints, and reflection](https://redteamer.tips/a-tale-of-net-assemblies-cobalt-strike-size-constraints-and-reflection/)
+18. [AppDomain.AssemblyResolve](https://offensivedefence.co.uk/posts/assembly-resolve/)
+19. [?webshell??????????????](https://mp.weixin.qq.com/s/mNCROss5pa4rkrWIIfjVfQ)
+20. [?Cobalt Strike BOF?????????](https://mp.weixin.qq.com/s/TLyQOupzep1BN7_nbjCXeQ)
+21. [Using Direct Syscalls in Cobalt Strike's Artifact Kit](https://www.youtube.com/watch?v=mZyMs2PP38w&feature=youtu.be&ab_channel=RaphaelMudge)
+22. [Cobalt Strike Staging and Extracting Configuration Information](https://blog.securehat.co.uk/cobaltstrike/extracting-config-from-cobaltstrike-stager-shellcode)
+
+#### 4. CobaltStrike Hide
+1. [CobaltStrike??????????](https://mp.weixin.qq.com/s/sYfvD0XQqi6BFw70_jrv5Q)
+2. [CS ???? + Powershell ??](http://blog.leanote.com/post/snowming/6a724671de78)
+3. [Cobalt Strike ???????](http://blog.leanote.com/post/snowming/d5d2b4ba20d0)
+4. [??????:????C2 server](https://xz.aliyun.com/t/4509)
+5. [Cobalt Strike HTTP C2 Redirectors with Apache mod_rewrite](https://bluescreenofjeff.com/2016-06-28-cobalt-strike-http-c2-redirectors-with-apache-mod_rewrite/)
+6. [????cobalt strike malleable C2????](https://xz.aliyun.com/t/2796)
+7. [A Brave New World: Malleable C2](http://www.harmj0y.net/blog/redteaming/a-brave-new-world-malleable-c2/)
+8. [How to Write Malleable C2 Profiles for Cobalt Strike](https://bluescreenofjeff.com/2017-01-24-how-to-write-malleable-c2-profiles-for-cobalt-strike/)
+9. [Randomized Malleable C2 Profiles Made Easy](https://bluescreenofjeff.com/2017-08-30-randomized-malleable-c2-profiles-made-easy/)
+10. [??CobaltStrike?Stager????](https://mp.weixin.qq.com/s/0MPM3bysJJYr5jbRnES_Vg)
+11. [Beacon Stager listener ???](https://mp.weixin.qq.com/s/HibtLfikI_0ezcLVCRxqaA)
+12. [?????Cobaltstrike???](https://hosch3n.github.io/2020/12/16/%E6%A3%80%E6%B5%8B%E4%B8%8E%E9%9A%90%E8%97%8FCobaltstrike%E6%9C%8D%E5%8A%A1%E5%99%A8/)
+13. [???cs bypass????????](https://xz.aliyun.com/t/9224)
+14. [Cobalt Strike � Bypassing C2 Network Detections](https://newtonpaul.com/cobalt-strike-bypassing-c2-network-detections/)
+
+ ''')
+f.close()
+
+
+subprocess.call('rm -rf file.py && \
+ git commit -am "🌱 you see me " && \
+ git push origin main', shell=True)
